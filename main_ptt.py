dde9afe46faf6e27aafa98f7f2801726553280cc
kylls Bellies <kylls.ccs@gmail.com>
2021-03-08 13:17:18 +0800

    modify to add db connection

--- main_ptt.py
+++ main_ptt.py
@@ -5,7 +5,6 @@ import time
 import pandas as pd
 import openpyxl
 import sqlite3
-import random
 
 class FetchPtt():
     def __init__(self, path, n, timerange):
@@ -19,7 +18,7 @@ class FetchPtt():
     # 取出需要爬取的頁數
     def page_link(self):
         response = requests.get(self.path, cookies = self.cookies)
-        data_soup = BeautifulSoup(response.text, 'lxml')
+        data_soup = BeautifulSoup(response.text, 'html.parser')
         prev_link_part = data_soup.select('.btn-group.btn-group-paging a')
         page_link_list = [i.get('href') for i in prev_link_part]
         prev_page_link = f'https://www.ptt.cc{page_link_list[1]}'
@@ -43,7 +42,7 @@ class FetchPtt():
         write_num = 0
 
         # =============叫出資料庫時間區間============
-        DB_file = '/home/kyllsbellies/文件/DB_local/0000A1_pttdata.db'
+        DB_file = './0000A1_pttdata.db'
         con = sqlite3.connect(DB_file)
         cur = con.cursor()
         data_bank = cur.execute(f'SELECT PostNumb FROM PTTDATA WHERE PostTims>{now_timestamp-self.timerange*20} AND PostBord=\'{self.bname}\';')
@@ -54,7 +53,7 @@ class FetchPtt():
         compare_box = []
         for j in path_list:
             response = requests.get(j, cookies = self.cookies)
-            data_soup = BeautifulSoup(response.text, 'lxml')
+            data_soup = BeautifulSoup(response.text, 'html.parser')
             article_bank = (i for i in data_soup.select('.r-ent')) # 做成generator
 
             for k in range(len(data_soup.select('.r-ent'))): # 每一個generator動作
@@ -84,9 +83,10 @@ class FetchPtt():
                                 pass
                 else:
                     delete_counter += 1
-        print(f'Status : 資料寫入數量 {write_num}.')
-        print(f'Status : 資料重複數量 {data_exist}.')
-        print(f'Status : 文章刪除數量 {delete_counter}.')
+        print(f'Status : 目前抓取版面: {self.bname}.')
+        print(f'Status : 資料寫入數量: {write_num}.')
+        print(f'Status : 資料重複數量: {data_exist}.')
+        print(f'Status : 文章刪除數量: {delete_counter}.')
         
         # ========可轉成excel存檔=============
         # df = pd.DataFrame(save_data_list, columns = ['PostNum', 'timeStamp', 'Boardname', 'Date', 'Author', 'Title', 'Link']).sort_values(by=['timeStamp'])
@@ -94,7 +94,7 @@ class FetchPtt():
         # df.to_excel('./test.xlsx', engine = 'openpyxl', index = None)
         
         # ========連結sqlite資料庫寫入=========
-        DB_file = '/home/kyllsbellies/文件/DB_local/0000A1_pttdata.db'
+        DB_file = './0000A1_pttdata.db'
         con = sqlite3.connect(DB_file)
         cur = con.cursor()
         cur.executemany('INSERT INTO PTTDATA VALUES(?,?,?,?,?,?,?)', save_data_list)
@@ -103,15 +103,29 @@ class FetchPtt():
 
 if __name__ == '__main__':
     # path = 'https://www.ptt.cc/bbs/Wanted/index.html'
-    path = 'https://www.ptt.cc/bbs/Sex/index.html'
+    # path = 'https://www.ptt.cc/bbs/Sex/index.html'
     # path = 'https://www.ptt.cc/bbs/feminine_sex/index.html'
     # path = 'https://www.ptt.cc/bbs/StupidClown/index.html'
     # path = 'https://www.ptt.cc/bbs/joke/index.html'
     # path = 'https://www.ptt.cc/bbs/beauty/index.html'
     # path = 'https://www.ptt.cc/bbs/C_Chat/index.html'
     # path = 'https://www.ptt.cc/bbs/Gossiping/index.html'
-    pageNumber = input('請輸入抓取頁數 : ')
-    timeSetting = input('請輸入監控分鐘數 : ')
-    a = FetchPtt(path, int(pageNumber), int(timeSetting))
-    b = a.page_link()
-    a.ptt_response(b)
+    # pageNumber = input('請輸入抓取頁數 : ')
+    # timeSetting = input('請輸入監控分鐘數 : ')
+    # a = FetchPtt(path, int(pageNumber), int(timeSetting))
+    # a = FetchPtt(path, 5, 30)
+    # b = a.page_link()
+    # a.ptt_response(b)
+
+    DB_path = './0000A1_pttdata.db'
+    con = sqlite3.connect(DB_path)
+    cur = con.cursor()
+
+    inner_sets_val = cur.execute('SELECT WEBSITE,PAGES,TIMES FROM PTTSCRAPY')
+
+    setting_c = [i for i in inner_sets_val]
+
+    for i in range(len(setting_c)):
+        a = FetchPtt(setting_c[i][0], setting_c[i][1], setting_c[i][2])
+        b = a.page_link()
+        a.ptt_response(b)
